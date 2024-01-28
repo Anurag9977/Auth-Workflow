e160a8308efafcecf3cd59a81b303d6e0b897a5c
ANURAG CHOUDHURY <anuragchoudhury852@gmail.com>
2024-01-29 01:14:09 +0530

    Updated with minor changes

--- public/app.js
+++ public/app.js
@@ -17,7 +17,7 @@ showMe();
 
 logoutBtn.addEventListener("click", async () => {
   try {
-    await axios.get("http://localhost:5000/api/v1/auth/logout");
+    await axios.get("/api/v1/auth/logout");
     showMe();
   } catch (error) {
     alert(error.response.data.message);
